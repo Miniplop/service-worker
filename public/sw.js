f2576dfabfedfc22c073cbf435bc9afd4ca33442
Miniplop <kevinj@theodo.fr>
2019-07-12 16:31:29 +0200

    Step 5 : Offline First

--- public/sw.js
+++ public/sw.js
@@ -8,16 +8,17 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith( async function () {
-      try {
-        const networkResponse =  await fetch(event.request)
-        const cache = await caches.open('airbnb-dynamic');
-        event.waitUntil(
-          cache.put(event.request, networkResponse.clone())
-        );
-        return networkResponse;
-      } catch (err) {
-        return caches.match(event.request)
-      }
+  event.respondWith( async function() {
+    const cachedResponse = caches.match(event.request);
+
+    return cachedResponse || async function() {
+      const networkResponse =  fetch(event.request);
+      const cache = await caches.open('airbnb-dynamic');
+      event.waitUntil(
+        cache.put(event.request, networkResponse.clone())
+      );
+
+      return networkResponse;
+    }
   }());
 });
